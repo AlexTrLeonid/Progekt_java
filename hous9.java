e006d63e5bc14f3d73cc18f816fe55874f730da1
AlexandrT <123582688+AlexTrLeonid@users.noreply.github.com>
2023-07-12 01:00:58 +0400

    Семинар 5 Задача hous9.java

--- hous9.java
+++ hous9.java
@@ -1,32 +1,90 @@
-import java.util.ArrayDeque;
-import java.util.Arrays;
-import java.util.Deque;
+// Задание
+// Реализуйте структуру телефонной книги с помощью HashMap.
+// Программа также должна учитывать, что во входной структуре будут 
+// повторяющиеся имена с разными телефонами, их необходимо считать,
+//  как одного человека с разными телефонами. 
+//  Вывод должен быть отсортирован по убыванию числа телефонов.
+
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+
 public class hous9 {
     public static void main(String[] args) {
-        Deque<Integer> d1 = new ArrayDeque<>(Arrays.asList(1,-2,3));
-        Deque<Integer> d2 = new ArrayDeque<>(Arrays.asList(5,-4,7));
-        // result [6,-6,0,1]
-        hous8 task = new hous8();
-         Deque<Integer> result = task.sum(d1, d2);
-        System.out.println(result);
+        Map<String, List<String>> phonBook = new HashMap<>();
+        phonBook.put("Lena", List.of("8 926 333 33 33", "8 926 222 22 99"));
+        phonBook.put("Ira", List.of("8 926 333 33 55", "8 926 222 22 00"));
+        phonBook.put("Igor", List.of("8 926 333 33 66", "8 926 222 22 44"));
+        phonBook.put("Petr", List.of("8 926 333 33 77", "8 926 222 22 11"));
+        phonBook.put("Dima", List.of("8 926 333 33 88", "8 926 222 22 22"));
+
+        menu(phonBook);
+
     }
-    public Deque<Integer> sum(Deque<Integer> d1, Deque<Integer> d2) {
-        Deque<Integer> result = new ArrayDeque<>();
-        int carry = 0;
-         while (!d1.isEmpty() || !d2.isEmpty()) { 
-            int product = carry;
-            if (!d1.isEmpty()) {
-                product *= d1.removeFirst();
+
+    public static String scanner() {
+        Scanner scanner = new Scanner(System.in);
+        String scan = scanner.nextLine();
+        // scanner.close();
+        return scan;
+    }
+
+    public static void find(Map<String, List<String>> phonBook) {
+        System.out.println("Введите имя: ");
+        String name = scanner();
+        System.out.println(phonBook.get(name));
+    }
+
+    public static void allBook(Map<String, List<String>> phonBook) {
+        System.out.println(phonBook);
+
+    }
+
+    public static Map<String, List<String>> add(Map<String, List<String>> phonBook) {
+        System.out.println("Если хотите выйте хотите перестать вводить номера введите 'stop'");
+        System.out.println("Введите имя: ");
+        String name = scanner();
+        List<String> number = new ArrayList<>();
+        while (true) {
+            System.out.println("Введите номер: ");
+            String phon = scanner();
+            if (phon.equals("stop")) {
+                break;
+            } else {
+                number.add(phon);
             }
-            if (!d2.isEmpty()) {
-                product *= d2.removeFirst();
+        }
+        phonBook.put(name, number);
+
+        return phonBook;
+    }
+
+    public static Map<String, List<String>> menu(Map<String, List<String>> phonBook) {
+        System.out.println(
+                "Введите команду из списка: find - найти контакт, add - добавить контакт, all - показать всю телефонную книгу, exit - выйти");
+        while (true) {
+            String comands = scanner();
+            if (comands.equals("exit")) {
+                break;
+            } else {
+                switch (comands) {
+                    case "find":
+                        find(phonBook);
+                        break;
+                    case "add":
+                        add(phonBook);
+                        break;
+                    case "all":
+                        allBook(phonBook);
+                        break;
+                    default:
+                        break;
+                }
             }
-            result.addLast(product % 10);
-            carry = product / 10; 
         }
-        if (carry != 0) {
-            result.addLast(carry);
-        }
-        return result;
+        return phonBook;
     }
 }
